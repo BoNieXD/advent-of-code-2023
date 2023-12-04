4d1289ca6c27232e0cd5cba51580f09f40a4e52e
Michał Nowak <michalmikolajnowak36@gmail.com>
2023-12-04 20:07:03 +0100

    day04 without regex

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -7,8 +7,12 @@ const DAY: u8 = 4;
 fn main() {
     let input = read_input(DAY, InputType::Real).unwrap();
 
+    println!("Regex");
     println!("Part1: {}", part1(&input));
     println!("Part2: {}", part2(&input));
+    println!("\nNo regex");
+    println!("Part1 no regex: {}", part1_no_regex(&input));
+    println!("Part2  no regex: {}", part2_no_regex(&input));
 }
 
 #[timed]
@@ -50,6 +54,48 @@ fn part2(input: &str) -> usize {
     coppied_cards.iter().sum()
 }
 
+#[timed]
+fn part1_no_regex(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|line| {
+            let mut line = line
+                .split(": ")
+                .last()
+                .unwrap()
+                .split('|')
+                .map(|card| card.split_ascii_whitespace());
+            let winning = line.next().unwrap().collect::<Vec<_>>();
+            line.next()
+                .unwrap()
+                .filter(|x| winning.contains(x))
+                .enumerate()
+                .fold(0, |_, (index, _)| 1 << index)
+        })
+        .sum()
+}
+
+#[timed]
+fn part2_no_regex(input: &str) -> usize {
+    let mut coppied_cards = vec![1; input.lines().count()];
+    let parsed = input.lines().map(|line| {
+        let mut line = line
+            .split(": ")
+            .last()
+            .unwrap()
+            .split('|')
+            .map(|card| card.split_ascii_whitespace().collect::<Vec<_>>());
+        (line.next().unwrap(), line.next().unwrap())
+    });
+    for (index, (winning, choosen)) in parsed.enumerate() {
+        let matches = choosen.iter().filter(|x| winning.contains(x)).count();
+        for i in index + 1..=index + matches {
+            coppied_cards[i] += coppied_cards[index];
+        }
+    }
+    coppied_cards.iter().sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -65,9 +111,21 @@ mod tests {
         assert_eq!(result, expected);
     }
     #[test]
+    fn part1_no_regex_test() {
+        let expected = 13;
+        let result = part1_no_regex(&get_test_input(InputType::Test));
+        assert_eq!(result, expected);
+    }
+    #[test]
     fn part2_test() {
         let expected = 30;
         let result = part2(&get_test_input(InputType::Test));
+        assert_eq!(result, expected);
+    }
+    #[test]
+    fn part2_no_regex_test() {
+        let expected = 30;
+        let result = part2_no_regex(&get_test_input(InputType::Test));
         assert_eq!(result, expected);
     }
 }
