4cfce8a1987d0effaa48729e7a62d9f803bc0aa4
Michał Nowak <michalmikolajnowak36@gmail.com>
2023-12-15 12:47:46 +0100

    day12

--- src/bin/day12.rs
+++ src/bin/day12.rs
@@ -1,6 +1,6 @@
+use std::collections::HashMap;
+
 use aoc2023::{read_input, InputType};
-use itertools::Itertools;
-use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 use timed::timed;
 
 const DAY: u8 = 12;
@@ -9,78 +9,90 @@ fn main() {
     let input = read_input(DAY, InputType::Real).unwrap();
 
     println!("Part1: {}", part1(&input));
+    println!("Part2: {}", part2(&input));
 }
 
-fn parse(input: &str) -> Vec<(Vec<char>, Vec<usize>)> {
-    input
-        .lines()
-        .map(|line| {
-            let line = line.split_once(' ').unwrap();
-            (
-                line.0.chars().collect::<Vec<_>>(),
-                line.1
-                    .split(',')
-                    .flat_map(|x| x.parse::<usize>())
-                    .collect::<Vec<_>>(),
-            )
-        })
-        .collect::<Vec<_>>()
-}
-
-fn check_if_possible(solution: &Vec<char>, numbers: &Vec<usize>) -> bool {
-    let mut groups = vec![];
-    let mut last_dot = true;
-    for i in 0..solution.len() {
-        if solution[i] == '.' {
-            last_dot = true;
-            continue;
-        } else {
-            if last_dot {
-                groups.push(0);
-            }
-            *groups.last_mut().unwrap() += 1;
-            last_dot = false;
-        }
+fn get_possible_solutions(
+    symbols: &Vec<char>,
+    numbers: &Vec<usize>,
+    mem: &mut HashMap<(usize, usize, usize), usize>,
+    current_symbol: usize,
+    current_number: usize,
+    count: usize,
+) -> usize {
+    let key = (current_symbol, current_number, count);
+    if mem.contains_key(&key) {
+        return *mem.get(&key).unwrap();
     }
-
-    if groups.len() != numbers.len() {
-        return false;
+    if current_symbol == symbols.len() {
+        return (current_number == numbers.len()
+            || (current_number == numbers.len() - 1 && count == numbers[current_number]))
+            as usize;
     }
-
-    for i in 0..groups.len() {
-        if groups[i] != numbers[i] {
-            return false;
-        }
-    }
-    true
-}
-
-fn generate_possible_solutions(symbols: &Vec<char>, numbers: &Vec<usize>) -> usize {
-    if symbols.into_iter().filter(|&&x| x != '.').count() < numbers.into_iter().sum() {
-        return 0;
-    }
-    if symbols.iter().all(|&x| x != '?') {
-        if check_if_possible(&symbols, numbers) {
-            return 1;
-        }
-        return 0;
+    if current_number == numbers.len() {
+        return (symbols[current_symbol..].iter().all(|&x| x != '#')) as usize;
     }
     let mut result = 0;
-    let mut symbols = symbols.clone();
-    let (index, _) = symbols.iter().find_position(|&&x| x == '?').unwrap();
-    symbols[index] = '.';
-    result += generate_possible_solutions(&symbols, numbers);
-    symbols[index] = '#';
-    result += generate_possible_solutions(&symbols, numbers);
+    if symbols[current_symbol] == '.' || symbols[current_symbol] == '?' {
+        if count == 0 || count == numbers[current_number] {
+            result += get_possible_solutions(
+                symbols,
+                numbers,
+                mem,
+                current_symbol + 1,
+                current_number + (count != 0) as usize,
+                0,
+            )
+        }
+    }
+    if symbols[current_symbol] == '#' || symbols[current_symbol] == '?' {
+        if count < numbers[current_number] {
+            result += get_possible_solutions(
+                symbols,
+                numbers,
+                mem,
+                current_symbol + 1,
+                current_number,
+                count + 1,
+            )
+        }
+    }
+    mem.insert(key, result);
     result
 }
 
 #[timed]
 fn part1(input: &str) -> usize {
-    let parsed = parse(input);
-    parsed
-        .par_iter()
-        .map(|(symbols, numbers)| generate_possible_solutions(symbols, numbers))
+    input
+        .lines()
+        .map(|line| {
+            let line = line.split_once(' ').unwrap();
+            let symbols = line.0.chars().collect::<Vec<_>>();
+            let numbers = line
+                .1
+                .split(',')
+                .flat_map(|x| x.parse::<usize>())
+                .collect::<Vec<_>>();
+            get_possible_solutions(&symbols, &numbers, &mut HashMap::new(), 0, 0, 0)
+        })
+        .sum()
+}
+
+#[timed]
+fn part2(input: &str) -> usize {
+    input
+        .lines()
+        .map(|line| {
+            let line = line.split_once(' ').unwrap();
+            let mut symbols = format!("{}?", line.0).repeat(5).chars().collect::<Vec<_>>();
+            symbols.pop();
+            let numbers = format!("{},", line.1)
+                .repeat(5)
+                .split(',')
+                .filter_map(|x| x.parse::<usize>().ok())
+                .collect::<Vec<_>>();
+            get_possible_solutions(&symbols, &numbers, &mut HashMap::new(), 0, 0, 0)
+        })
         .sum()
 }
 
@@ -96,6 +108,12 @@ mod tests {
     fn part1_test() {
         let expected = 21;
         let result = part1(&get_test_input(InputType::Test));
+        assert_eq!(result, expected);
+    }
+    #[test]
+    fn part2_test() {
+        let expected = 525152;
+        let result = part2(&get_test_input(InputType::Test));
         assert_eq!(result, expected);
     }
 }
